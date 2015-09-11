f3e11f9ec34d4fad9bbc4a021ef1ec20390738f1
benwa <btellier@linagora.com>
2015-09-11 16:00:27 +0200

    JAMES-1607 Integrate MailboxCopierManagement changes with Spring

--- container/mailbox-adapter/src/main/java/org/apache/james/adapter/mailbox/MailboxCopierManagement.java
+++ container/mailbox-adapter/src/main/java/org/apache/james/adapter/mailbox/MailboxCopierManagement.java
@@ -23,6 +23,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import org.apache.james.mailbox.MailboxManager;
 import org.apache.james.mailbox.copier.MailboxCopier;
