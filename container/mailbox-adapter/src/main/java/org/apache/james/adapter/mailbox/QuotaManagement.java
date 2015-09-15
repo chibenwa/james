09b1f2e3b1531910db8098e7f46d20a41586e55d
benwa <btellier@linagora.com>
2015-09-16 01:18:17 +0200

    JAMES-511 Introduce QuotaManagement interfaces

---
+++ container/mailbox-adapter/src/main/java/org/apache/james/adapter/mailbox/QuotaManagement.java
@@ -0,0 +1,106 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.adapter.mailbox;
+
+import org.apache.james.mailbox.exception.MailboxException;
+import org.apache.james.mailbox.model.MailboxPath;
+import org.apache.james.mailbox.model.Quota;
+import org.apache.james.mailbox.model.QuotaRoot;
+import org.apache.james.mailbox.quota.MaxQuotaManager;
+import org.apache.james.mailbox.quota.QuotaManager;
+import org.apache.james.mailbox.quota.QuotaRootResolver;
+import org.apache.james.mailbox.store.quota.QuotaRootImpl;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+public class QuotaManagement implements QuotaManagementMBean {
+
+    private QuotaManager quotaManager;
+    private MaxQuotaManager maxQuotaManager;
+    private QuotaRootResolver quotaRootResolver;
+
+    public void setQuotaManager(QuotaManager quotaManager) {
+        this.quotaManager = quotaManager;
+    }
+
+    public void setMaxQuotaManager(MaxQuotaManager maxQuotaManager) {
+        this.maxQuotaManager = maxQuotaManager;
+    }
+
+    public void setQuotaRootResolver(QuotaRootResolver quotaRootResolver) {
+        this.quotaRootResolver = quotaRootResolver;
+    }
+
+    @Override
+    public QuotaRoot getQuotaRoot(String namespace, String user, String name) throws MailboxException {
+        return quotaRootResolver.getQuotaRoot(new MailboxPath(namespace, user, name));
+    }
+
+    @Override
+    public long getMaxMessageCount(String quotaRoot) throws MailboxException {
+        return maxQuotaManager.getMaxMessage(quotaRootResolver.createQuotaRoot(quotaRoot));
+    }
+
+    @Override
+    public long getMaxStorage(String quotaRoot) throws MailboxException {
+        return maxQuotaManager.getMaxStorage(quotaRootResolver.createQuotaRoot(quotaRoot));
+    }
+
+    @Override
+    public long getDefaultMaxMessageCount() throws MailboxException {
+        return maxQuotaManager.getDefaultMaxMessage();
+    }
+
+    @Override
+    public long getDefaultMaxStorage() throws MailboxException {
+        return maxQuotaManager.getDefaultMaxStorage();
+    }
+
+    @Override
+    public void setMaxMessageCount(String quotaRoot, long maxMessageCount) throws MailboxException {
+        maxQuotaManager.setMaxMessage(quotaRootResolver.createQuotaRoot(quotaRoot), maxMessageCount);
+    }
+
+    @Override
+    public void setMaxStorage(String quotaRoot, long maxSize) throws MailboxException {
+        maxQuotaManager.setMaxStorage(quotaRootResolver.createQuotaRoot(quotaRoot), maxSize);
+    }
+
+    @Override
+    public void setDefaultMaxMessageCount(long maxDefaultMessageCount) throws MailboxException {
+        maxQuotaManager.setDefaultMaxMessage(maxDefaultMessageCount);
+    }
+
+    @Override
+    public void setDefaultMaxStorage(long maxDefaultSize) throws MailboxException {
+        maxQuotaManager.setDefaultMaxStorage(maxDefaultSize);
+    }
+
+    @Override
+    public Quota getMessageCountQuota(String quotaRoot) throws MailboxException {
+        return quotaManager.getMessageQuota(quotaRootResolver.createQuotaRoot(quotaRoot));
+    }
+
+    @Override
+    public Quota getStorageQuota(String quotaRoot) throws MailboxException {
+        return quotaManager.getStorageQuota(quotaRootResolver.createQuotaRoot(quotaRoot));
+    }
+}
